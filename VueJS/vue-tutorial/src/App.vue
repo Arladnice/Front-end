972c4282cc8edd8ba9e7942777fe9933e5bd1e31
arladnice <darlakov@mail.ru>
2020-11-30 15:48:46 +0300

    testTask-begin

---
+++ VueJS/vue-tutorial/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <PlusMinus />
+  </div>
+</template>
+
+<script>
+
+  import PlusMinus from "./components/PlusMinus";
+
+export default {
+  name: 'App',
+  components: {
+    PlusMinus
+  }
+}
+</script>
---
+++ VueJS/vue-tutorial/src/components/PlusMinus.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h3>Я найду тебя!</h3>
+        <input type="text" @input="searchText = $event.target.value">
+        <p>Всего имён: {{users.length}}, совпадений: {{getFilteredItems.length}}</p>
+        <ul>
+            <li v-for="(name, index) in getFilteredItems" :key="index"
+            >
+                {{ name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                searchText: '',
+                users: ['Денис', 'Саша', 'Костя', 'Андрей'],
+            }
+        },
+        computed: {
+            getFilteredItems() {
+                return this.users.filter(name => name.toLowerCase().includes(this.searchText.toLowerCase()))
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
