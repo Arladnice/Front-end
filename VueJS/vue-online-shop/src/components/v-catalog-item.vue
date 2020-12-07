c33755ce480bf7f9ed351ecd74957cc5e43e906c
arladnice <darlakov@mail.ru>
2020-12-07 12:18:52 +0300

    vue-practice

---
+++ VueJS/vue-online-shop/src/components/v-catalog-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-catalog-item">
+    <p>Item 1</p>
+    <p>Price: 100</p>
+    <button>Add to cart</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "v-catalog-item"
+    }
+</script>
+
+<style lang="scss">
+  .v-catalog-item {
+    flex-basis: 25%;
+    box-shadow: 0 0 8px 0 #e0e0e0;
+    padding: $padding*24;
+  }
+</style>
