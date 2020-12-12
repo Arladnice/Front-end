a5eacb26447160dd7a05284a6e98a306ee86c2e7
arladnice <darlakov@mail.ru>
2020-12-12 19:49:50 +0300

    start validation

---
+++ VueJS/vue-crm-2/src/components/app/Sidebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul class="sidenav app-sidenav" :class="{open: value}">
+      <router-link
+        v-for="link in links"
+        :key="link.url"
+        tag="li"
+        active-class="active"
+        :to="link.url"
+        :exact="link.exact"
+      >
+        <a href="">{{ link.title }}</a>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            links: [
+                {title: 'Счет', url: '/', exact: true},
+                {title: 'История', url: '/history'},
+                {title: 'Планирование', url: '/planning'},
+                {title: 'Новая запись', url: '/record'},
+                {title: 'Категории', url: '/categories'},
+            ]
+        }),
+        props: ['value']
+    }
+</script>
+
+<style scoped>
+
+</style>
