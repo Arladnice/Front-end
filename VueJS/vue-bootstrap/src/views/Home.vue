4509a87eba173762bfdd1e76e5b9ef47357d446a
arladnice <darlakov@mail.ru>
2020-12-21 00:48:32 +0300

    21.12

---
+++ VueJS/vue-bootstrap/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container>
+    <b-row align-v="center">
+      <b-col>
+        <b-card
+        title="This is a card"
+        img-src=""
+        >
+          <b-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut debitis dolore enim harum id iusto molestias obcaecati, provident repellat!
+          </b-card-text>
+          <b-button variant="primary">Text</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
